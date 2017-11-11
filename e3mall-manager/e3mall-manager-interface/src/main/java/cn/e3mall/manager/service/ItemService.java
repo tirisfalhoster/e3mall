13bde55693f657db8127f895cd1b422da1b69bd6
wildcottier <mtreexml@outlook.com>
2017-11-11 14:06:55 +0800

    version04_fastDFS,图片上传,商品添加_2017年11月11日14:06:54

--- e3mall-manager/e3mall-manager-interface/src/main/java/cn/e3mall/manager/service/ItemService.java
+++ e3mall-manager/e3mall-manager-interface/src/main/java/cn/e3mall/manager/service/ItemService.java
@@ -1,14 +1,27 @@
 package cn.e3mall.manager.service;
 
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemDesc;
+import cn.e3mall.utils.E3mallResult;
 import cn.e3mall.utils.PageBeanResult;
 
 public interface ItemService {
 	
+	/**
+	 * 根据id查询商品数据.
+	 * @param itemId
+	 * @return
+	 */
 	public TbItem findItemById(Long itemId);
 	
 	/**
 	 * 分页查询商品列表.
 	 */
 	public PageBeanResult findItemByPage(Integer page, Integer rows);
+	
+	/**
+	 * 保存商品数据.
+	 * 参数TbItem item, TbItemDesc itemDesc
+	 */
+	public E3mallResult saveItem(TbItem item, TbItemDesc itemDesc);
 }
--- e3mall-manager/e3mall-manager-service/src/main/java/cn/e3mall/manager/service/impl/ItemServiceImpl.java
+++ e3mall-manager/e3mall-manager-service/src/main/java/cn/e3mall/manager/service/impl/ItemServiceImpl.java
@@ -1,5 +1,6 @@
 package cn.e3mall.manager.service.impl;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +10,13 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 
 import cn.e3mall.manager.service.ItemService;
+import cn.e3mall.mapper.TbItemDescMapper;
 import cn.e3mall.mapper.TbItemMapper;
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemDesc;
 import cn.e3mall.pojo.TbItemExample;
+import cn.e3mall.utils.E3mallResult;
+import cn.e3mall.utils.IDUtils;
 import cn.e3mall.utils.PageBeanResult;
 
 @Service
@@ -19,6 +24,9 @@ public class ItemServiceImpl implements ItemService {
 
 	@Autowired
 	private TbItemMapper itemMapper;
+	
+	@Autowired
+	private TbItemDescMapper itemDescMapper;
 	
 	@Override
 	public TbItem findItemById(Long itemId) {
@@ -42,5 +50,57 @@ public class ItemServiceImpl implements ItemService {
 		
 		return result;
 	}
+
+	//保存商品数据.
+	@Override
+	public E3mallResult saveItem(TbItem item, TbItemDesc itemDesc) {
+		long itemId = IDUtils.genItemId();
+		item.setId(itemId);
+		item.setStatus((byte)1);	//1-正常,2-下架, 3-删除,我们这是一个B2C商城,商品实际上是不需要审核的.都是我们自己上架的.
+		Date date = new Date();
+		item.setCreated(date);
+		item.setUpdated(date);
+		
+		itemMapper.insert(item);	//保存商品对象.
+		
+		itemDesc.setItemId(itemId);
+		itemDesc.setCreated(date);
+		itemDesc.setUpdated(date);
+		
+		itemDescMapper.insert(itemDesc);	//保存商品描述对象.
+		
+		return E3mallResult.ok();	//会自动调用那个E3mallResult的构造函数,构造函数中有定义值.
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
